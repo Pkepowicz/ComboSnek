cb31b42b0944f2df4f06e47de7fbb8c6900e0791
Pkepowicz <paw.kepowicz@gmail.com>
2022-12-05 13:52:11 +0100

    deleted vibrations

--- Assets/Scripts/Snek.cs
+++ Assets/Scripts/Snek.cs
@@ -101,24 +101,20 @@ public class Snek : MonoBehaviour
     public void Up()
     {
         if (direction != Vector3.down) desired = Vector2.up;
-        Handheld.Vibrate();
     }
 
     public void Down()
     {
         if (direction != Vector3.up) desired = Vector2.down;
-        Handheld.Vibrate();
     }    
 
     public void Left()
     {
         if (direction != Vector3.right) desired = Vector2.left;
-        Handheld.Vibrate();
     }
 
     public void Right()
     {
         if (direction != Vector3.left) desired = Vector2.right;
-        Handheld.Vibrate();
     }
 }
