1bdd1ef975267a89d6e8db08bea64c148464ebf7
ddziechciarz <alawon12@gmail.com>
2022-12-05 14:07:10 +0100

    working scene transition

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -18,14 +18,32 @@ public class LevelManager : MonoBehaviour
             DontDestroyOnLoad(this.gameObject);
         }
     }
+    
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+                activity.Call<bool>("moveTaskToBack", true);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
 
     public void LoadGame()
     {
         SceneManager.LoadScene("MainScene");
+        Time.timeScale = 1f;
     }
 
     public void LoadMenu()
     {
         SceneManager.LoadScene("MainMenu");
+        
     }
 }
