bb69db739b16f8deea30d8d054e22a236af76685
ddziechciarz <alawon12@gmail.com>
2022-12-05 16:56:27 +0100

    save system done

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Slider volumeSlider;
     
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
+        PlayerPrefs.SetFloat("volume", volume);
+    }
+
+    public void SetSliderValue(float value)
+    {
+        volumeSlider.value = value;
     }
 }
