dc79de1da1a3efcd45cce97b31da674c51f3d2ad
pkepowicz <paw.kepowicz@gmail.com>
2023-11-05 23:20:41 +0100

    huge load of changes

---
+++ Assets/LeaderboardCreator/Scripts/Main/Leaderboards.cs
@@ -0,0 +1,7 @@
+namespace Dan.Main
+{
+    public static class Leaderboards
+    {
+        public static LeaderboardReference ComboSnek = new LeaderboardReference("3029fb8e0e705517042a9a6f3e9d32893f876eb0b15ef3794273a291b5c930fa");
+    }
+}
